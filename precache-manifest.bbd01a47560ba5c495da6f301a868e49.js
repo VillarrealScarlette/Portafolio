6578165dca107d89235a51a32ba61a723eb0aadf
Scarlette Villarreal Á <villarrealscarlette@gmail.com>
2019-11-15 17:44:14 -0300

    Updates

---
+++ precache-manifest.bbd01a47560ba5c495da6f301a868e49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a5a5766d641afdd155ea42ea82c5fd2",
+    "url": "/Portafolio/index.html"
+  },
+  {
+    "revision": "3828116d78920e82cd74",
+    "url": "/Portafolio/static/css/2.d7a94725.chunk.css"
+  },
+  {
+    "revision": "d4284e705d59b708481d",
+    "url": "/Portafolio/static/css/main.0ebe4456.chunk.css"
+  },
+  {
+    "revision": "3828116d78920e82cd74",
+    "url": "/Portafolio/static/js/2.09d41813.chunk.js"
+  },
+  {
+    "revision": "d4284e705d59b708481d",
+    "url": "/Portafolio/static/js/main.b07d7632.chunk.js"
+  },
+  {
+    "revision": "741b59ccd88e70ea87a0",
+    "url": "/Portafolio/static/js/runtime-main.2a199dca.js"
+  }
+]);
