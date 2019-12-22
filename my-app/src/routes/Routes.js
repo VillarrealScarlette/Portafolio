6364b52285d74d0be46aa89c53d7637cd1267a5d
Scarlette Villarreal Á <villarrealscarlette@gmail.com>
2019-12-21 23:12:31 -0300

    add links to social networks

--- my-app/src/routes/Routes.js
+++ my-app/src/routes/Routes.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Home from '../views/Home';
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import AboutMe from '../views/AboutMe'
 import Projects from '../views/Projects';
 import Contact from '../views/Contact';
 
 const Routes = () => {
     return (
-        <Switch>
-            <Route exact path='/' component={Home}/>
-            <Route exact path='/AboutMe' component={AboutMe}/>
-            <Route exact path='/Projects' component={Projects}/>
-            <Route exact path='/Contact' component={Contact}/>
-        </Switch>
+        <Router>
+        <React.Fragment>
+          <Redirect
+              exact
+              from="/"
+              to="/AboutMe" />
+            <Switch>
+              <Route
+                path="/AboutMe"
+                component={AboutMe} />
+                <Route
+                path="/Proyectos"
+                component={Projects} />
+                <Route
+                path="/Contacto"
+                component={Contact} />
+            </Switch>
+          </React.Fragment>
+        </Router>
     )
 }
 
