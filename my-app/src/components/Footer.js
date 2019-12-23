3da4bdeec9b11fc1fb18db45939e56dd2eb17386
Scarlette Villarreal Á <villarrealscarlette@gmail.com>
2019-12-23 11:21:58 -0300

    add project images in view projects.js

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import './components.css';
 
 const Footer = () => {
     return (
-        <div>
+        <div style={{
+            display: "flex",
+            alignItems: "center",
+            width: "100%",
+            height: "150px",
+            justifyContent: "space-between",
+            padding: "0px 20px"
+        }}>
             <div>
             <p>2019 Scarlette Villarreal Ávila</p>
             </div>
-            <div>
-            <a alt="Logo" href="/AboutMe" src="/Users/ScarletteVillarreal/Documents/Laboratoria/Portafolio L/my-app/public/favicon.png"></a>
+            <div style={{
+                display: "flex",
+                justifyContent: "center",
+            }}>
+            <a className="nounderline logo-icon" href="/AboutMe"><button className="logo"></button></a>
+            </div>
+            <div  style={{
+                    display: "flex"
+            }}>
+                <a className="nounderline sn" href="https://github.com/VillarrealScarlette" target="_blank" rel="noopener noreferrer">GitHub</a>
+                <p>|</p>
+                <a className="nounderline sn" href="https://www.linkedin.com/in/scarlettevillarreal/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
             </div>
         </div>
     )
--- my-app/src/views/Contact.js
+++ my-app/src/views/Contact.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import { Card, CardTitle, CardText } from 'reactstrap';
 
-export default class Contact extends React.Component {
+export default class Projects extends React.Component {
     render() {
         return (
-            <Card body style={{ backgroundColor: '#fffff', borderColor: '#333' }}>
-            <CardTitle>
-                <h3>
-                    Contact Section
-                </h3>
+            <div style={{
+                textAlign: "-webkit-center",
+                marginTop: "10px"
+            }}>
+                <Card body style={{
+                    backgroundColor: "#F2E9EC",
+                    borderColor: "#333",
+                    width: "1173px",
+                    height: "710px",
+                    borderRadius: "55px 55px 0px 0px",
+                    border: "none",
+                }}>
+                <CardTitle style={{
+                    fontFamily: "Iowan Old Style",
+                    fontStyle: "normal",
+                    fontWeight: 900,
+                    color: "#111C11",
+                    fontSize: "45px",
+                    width: "610px",
+                    height: "204px",
+                    textAlign: "-webkit-left",
+                    position: "absolute",
+                    margin: "165px 80px"
+                    }}>
+                    Porqué trabajar conmigo?
             </CardTitle>
-            <CardText>Contact Text</CardText>
+            <CardText style={{
+                        width: "540px",
+                        height: "180px",
+                        position: "absolute",
+                        margin: "425px 80px",
+                        textAlign: "initial",
+                        fontSize: "27px",
+                }}>Razones <br></br> Contacto</CardText>
             </Card>
+
+            </div>
         )
     }
 }
